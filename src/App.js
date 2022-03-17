efa5a3775d563c44d16b51dbe91c83d0163fbd06
George Fraiha <george_fraiha@hotmail.com>
2022-03-17 14:01:56 +0200

    working on Contact

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
+import Contact from "./pages/Contact";
 import Listing from "./pages/Listing";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
@@ -32,6 +33,7 @@ function App() {
             path="/category/:categoryName/:listingId"
             element={<Listing />}
           />
+            <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
