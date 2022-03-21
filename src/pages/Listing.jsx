25fe7b4894989e5f19bedcc28fa2fc7e4546babb
George Fraiha <george_fraiha@hotmail.com>
2022-03-21 22:21:24 +0200

    adding changes

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,6 +3,12 @@ import {useState, useEffect} from 'react'
 import {Link, useNavigate, useParams} from 'react-router-dom'
 import {getDoc, doc} from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+
+
 import {db} from '../firebase.config'
 import Spinner from '../components/Spinner'
 import ShareIcon from '../assets/svg/shareIcon.svg'
@@ -33,11 +39,26 @@ useEffect(()=>{
 
     fetchListing()
 },[navigate, params.listingId])
+
+
 if (loading){
     return <Spinner />
 }
   return <main> 
-{/* Slider goes here */}
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imgUrls.map((url, index) => (
+            
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+              }}
+              className='swiperSlideDiv'
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 <div className="shareIconDiv" onClick={()=>{
     navigator.clipboard.writeText(window.location.href)
     setShareLinkCopied(true)
@@ -75,10 +96,23 @@ if (loading){
      </ul>
 
      <p className="listingLocationTitle">Location</p>
-     {/*Map */}
+    <div className="leafletContainer">
+        <MapContainer style={{height:'100%', width: '100%'}} 
+        center={[listing.geolocation.lat,listing.geolocation.lng]} 
+        zoom={13} scrollWheelZoom={false}> 
+        <TileLayer 
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'/>
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+        </MapContainer>
+    </div>
 
      {auth.currentUser?.uid !== listing.userRef && (
-         <Link to={`/conatct/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`} className='primaryButton' >Contact Landlord</Link>
+         <Link to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`} className='primaryButton' >Contact Landlord</Link>
      )  }
 </div>
 
