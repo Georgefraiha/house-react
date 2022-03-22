21c6104339d5da618b18e8708545531c8d38f72d
George Fraiha <george_fraiha@hotmail.com>
2022-03-22 22:59:43 +0200

    adding slider

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -4,15 +4,15 @@ import {Link, useNavigate, useParams} from 'react-router-dom'
 import {getDoc, doc} from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
-
-
 import {db} from '../firebase.config'
 import Spinner from '../components/Spinner'
 import ShareIcon from '../assets/svg/shareIcon.svg'
 import { async } from '@firebase/util'
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
     const [listing, setListing]=useState(null)
@@ -53,6 +53,7 @@ if (loading){
               style={{
                 background: `url(${listing.imgUrls[index]}) center no-repeat`,
                 backgroundSize: 'cover',
+                height: '200px'
               }}
               className='swiperSlideDiv'
             ></div>
